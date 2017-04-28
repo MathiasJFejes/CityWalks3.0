2ecff8a218b562fc16b54f39486d8526a433b2ef
MathiasJFejes <mathias.johannes.fejes@gmail.com>
2017-04-28 11:52:18 +0200

    Add service

--- CityWalks3.0/www/js/services.js
+++ CityWalks3.0/www/js/services.js
@@ -4,6 +4,21 @@ angular.module('app.services', [])
 
 }])
 
+.factory('myService', function(){
+    var savedData = {}
+    function set(data) {
+        savedData = data;
+    }
+    function get() {
+        return savedData;
+    }
+
+    return {
+        set: set,
+        get: get
+    }
+})
+
 .service('BlankService', [function(){
 
 }]);
